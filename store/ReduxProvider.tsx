9e7437def7d505bf773ed28694469c7dc6fa37d1
teodora <tbdimitrova.bg@gmail.com>
2025-12-19 23:48:18 +0200

    refactor: simplify ReduxProvider component

--- store/ReduxProvider.tsx
+++ store/ReduxProvider.tsx
@@ -1,19 +1,11 @@
 "use client";
 import { Provider } from "react-redux";
-import { useEffect } from "react";
-import { store, persistor } from "./store";
+import { store } from "./store";
 
 export default function ReduxProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    if (persistor && typeof window !== "undefined") {
-      const unsubscribe = persistor.subscribe(() => {});
-      return () => unsubscribe();
-    }
-  }, []);
-
   return <Provider store={store}>{children}</Provider>;
 }
